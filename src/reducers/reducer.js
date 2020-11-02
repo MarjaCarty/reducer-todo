cbe7f09c657691037d408cc91fa31e5f7f08cb81
MarjaCarty <marja.d.carty@gmail.com>
2020-11-02 14:14:25 -0600

    build reducer skeleton

---
+++ src/reducers/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  todoList: [],
+  todo: { text: "", completed: false, id: "" },
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return { state };
+  }
+};
+
+export default reducer;
